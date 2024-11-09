b776e1f034b9d7171b8daef8fc205a38c98403e3
PamBWillenz <pam@positek.net>
2024-11-09 11:27:03 -0500

    add methods to merchant model to show return sums and average returns in 2 weeks

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,22 @@
 class Merchant < ApplicationRecord
   has_many :customer_returns
 end
+
+def sum_of_returned_amounts
+  customer_returns.joins(:items).sum('items.price')
+end
+# This method calculates the total return amounts 
+# by summing up the prices of all items associated with the customer returns
+
+def average_return_window
+  recent_returns = customer_returns.where('registered_date > ?', 14.days.ago)
+  return '-' if recent_returns.empty?
+
+  total_days = recent_returns.sum do |return|
+    (return.registered_date - return.order_date).to_i
+  end
+
+  total_days / recent_returns.count
+end
+# This method calculates the average return window in days for returns created in the last 14 days. 
+# It calculates the difference in days between the registered_date and order_date for each return and then averages these values.
